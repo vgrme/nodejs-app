75beb3b28251450ce14dd6a9870f404f03d92f97
VR <vinay.g.rajagopal@gmail.com>
2022-03-26 00:56:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(require('pino-http')({logger: pino}));
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
   req.log.info({message: 'Hello Vinay! Your app has been deployed successfully using Node.js Starter Application!'});
-  res.send('Hello from Node.js Starter Application!');
+  res.send('Hello Vinay! Your app has been deployed successfully using Node.js Starter Application!');
 });
 
 app.get('*', (req, res) => {
